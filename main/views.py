1daf389aee1a44ee924136b10b33e062aa024691
Shubham Dokania <skd.1810@gmail.com>
2016-10-22 12:14:39 +0530

    registration function view

--- main/views.py
+++ main/views.py
@@ -1,4 +1,39 @@
 from django.shortcuts import render
 
-# Create your views here.
+from models import UserProfile
+from forms import UserProfileForm, UserForm
 
+
+def register(request):
+
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileForm(data=request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save()
+
+            profile.user = user
+            profile.save()
+
+            registered = True
+        else:
+            print user_form.errors, profile_form.errors
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    context_dict = {
+        'user_form': user_form,
+        'profile_form': profile_form,
+        'registered': registered
+    }
+
+    return render(request, "register.html", context_dict)
