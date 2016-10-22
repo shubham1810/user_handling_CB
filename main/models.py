6be8384c71351b891956adec1bb053521a8fc612
Shubham Dokania <skd.1810@gmail.com>
2016-10-22 12:00:20 +0530

    Form registry

--- main/models.py
+++ main/models.py
@@ -2,3 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User)
+    website = models.URLField(blank=True)
+    score = models.IntegerField(blank=True)
+
+    def __unicode__(self):
+        return self.user.username
